3691f86e063b7581ec87db7a613957b58a2d607d
Azinth <gsv.jesus@gmail.com>
2024-11-16 21:20:09 -0300

    adiciona sctipts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 function Home() {
-  return <h1>Jacqueline, eu gosto muito de você e estou fazendo essa página especialmente para você. Se você gostou, dê uma risadinha ☺️
-</h1>;
+  return (
+    <h1>
+      Jacqueline, eu gosto muito de você e estou fazendo essa página
+      especialmente para você. Se você gostou, dê uma risadinha ☺️
+    </h1>
+  );
 }
 
 export default Home;
