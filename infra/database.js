931ce8616a4d449f800dbc0199e766ae814bcd69
Azinth <gsv.jesus@gmail.com>
2025-03-03 01:05:49 -0300

    differ dev enviroment to production

--- infra/database.js
+++ infra/database.js
@@ -1,15 +1,24 @@
 import { Client } from 'pg';
 
 async function query(queryObject) {
-    const client = new Client({
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD,
-        ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
-        pool_mode: "transaction",
-    });
+    // Configuração condicional com base no ambiente
+    const clientConfig = process.env.NODE_ENV === 'production'
+        ? {
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+              rejectUnauthorized: false // Necessário para conexões SSL com Supabase
+            }
+          }
+        : {
+            // Configuração de desenvolvimento local
+            host: process.env.POSTGRES_HOST,
+            port: process.env.POSTGRES_PORT,
+            user: process.env.POSTGRES_USER,
+            database: process.env.POSTGRES_DB,
+            password: process.env.POSTGRES_PASSWORD,
+          };
+    
+    const client = new Client(clientConfig);
     
     try {
       await client.connect();
@@ -24,5 +33,5 @@ async function query(queryObject) {
 }
 
 export default {
-    query:query,
+    query: query,
 };
