1be8ffa500acde8494bf46c28d68b2064e4e0f59
Azinth <gsv.jesus@gmail.com>
2025-03-07 17:40:44 -0300

    adds `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,31 +1,39 @@
 import { Client } from 'pg';
+import { config } from 'dotenv';
+config({ path: '.env.development' });
 
-async function query(queryObject) {
-    // Configuração condicional com base no ambiente
-    const clientConfig = process.env.NODE_ENV === 'production'
-        ? {
-            connectionString: process.env.DATABASE_URL,
-            ssl: {
-              rejectUnauthorized: false // Necessário para conexões SSL com Supabase
-            }
+export async function getNewClient() {
+  // Define a configuração do cliente com base no ambiente
+  const clientConfig = process.env.NODE_ENV === 'production'
+      ? {
+          connectionString: process.env.DATABASE_URL,
+          ssl: {
+            rejectUnauthorized: false // Necessário para conexões SSL com Supabase
           }
-        : {
-            // Configuração de desenvolvimento local
-            host: process.env.POSTGRES_HOST,
-            port: process.env.POSTGRES_PORT,
-            user: process.env.POSTGRES_USER,
-            database: process.env.POSTGRES_DB,
-            password: process.env.POSTGRES_PASSWORD,
-          };
-    
-    const client = new Client(clientConfig);
-    
+        }
+      : {
+          // Configuração de desenvolvimento local
+          host: process.env.POSTGRES_HOST,
+          port: process.env.POSTGRES_PORT,
+          user: process.env.POSTGRES_USER,
+          database: process.env.POSTGRES_DB,
+          password: process.env.POSTGRES_PASSWORD,
+        };
+
+  // Cria uma nova instância de Client usando a configuração definida
+  const client = new Client(clientConfig);
+  await client.connect();
+  return client;
+}
+
+export async function query(queryObject) {
+    let client;
     try {
-      await client.connect();
+      client = await getNewClient();
       const result = await client.query(queryObject);
       return result;
     } catch (error) {
-        console.log(error);
+        console.error(error);
         throw error;
     } finally {
         await client.end();
@@ -33,5 +41,6 @@ async function query(queryObject) {
 }
 
 export default {
-    query: query,
+    query,
+    getNewClient
 };
