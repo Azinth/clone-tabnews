630865d1db8bd88ad3e3c3ee773a3af077d92e1b
Azinth <gsv.jesus@gmail.com>
2025-03-08 23:14:36 -0300

    ci: add a cloud environment verification to use `connectionString`

--- infra/database.js
+++ infra/database.js
@@ -4,21 +4,21 @@ config({ path: '.env.development' });
 
 export async function getNewClient() {
   // Define a configuração do cliente com base no ambiente
-  const clientConfig = process.env.NODE_ENV === 'production'
-      ? {
-          connectionString: process.env.DATABASE_URL,
-          ssl: {
-            rejectUnauthorized: false // Necessário para conexões SSL com Supabase
-          }
-        }
-      : {
-          // Configuração de desenvolvimento local
-          host: process.env.POSTGRES_HOST,
-          port: process.env.POSTGRES_PORT,
-          user: process.env.POSTGRES_USER,
-          database: process.env.POSTGRES_DB,
-          password: process.env.POSTGRES_PASSWORD,
-        };
+  const clientConfig = isCloudEnvironment
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  : {
+      // Configuração de desenvolvimento local
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      user: process.env.POSTGRES_USER,
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD,
+    };
 
   // Cria uma nova instância de Client usando a configuração definida
   const client = new Client(clientConfig);
